7e4b1d31ff979d19f6a12dce436b198778bb07ad
svuruput <shravyavk@gmail.com>
2024-08-19 12:06:07 +0200

    RestAssuredShop Assignment

---
+++ src/test/java/Builder/Builder.java
@@ -0,0 +1,9 @@
+package Builder;
+
+public interface Builder {
+
+    ShopBuilder setDescription(String description);
+    ShopBuilder setId(int id);
+    ShopBuilder setName(String name);
+
+}
---
+++ src/test/java/dto/Shop.java
@@ -0,0 +1,41 @@
+package dto;
+
+public class Shop {
+
+    private String city;
+    private String country;
+    private String name;
+    private int id;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
